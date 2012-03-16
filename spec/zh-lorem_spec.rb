598dc26c5f32958fcb675977808c8d2ce1997f96
Kang-min Liu <gugod@gugod.org>
2012-03-17 00:04:14 +0800

    briefly test it.

--- spec/zh-lorem_spec.rb
+++ spec/zh-lorem_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ZhLorem" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "has instance methods" do
+    o = ZhLorem.new
+    s = o.sentence
+    s.should_not be_nil
+  end
+
+  it "has class methods" do
+    s = ZhLorem.sentence
+    s.should_not be_nil
   end
 end
